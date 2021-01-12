5c9d1bb70f865fb8bdafaeeaa861b6ae4ffe0e2f
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-01-12 15:15:28 +0900

    色関係の定数をColorSetクラスで定義

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205701/ColorSet.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e205701;
+
+import java.awt.Color;
+
+public class ColorSet {
+    static final Color BLACK = Color.BLACK;
+    static final Color SKYBLUE = Color.getHSBColor(0.54f, 1, 1);
+    static final Color YELLOW = Color.getHSBColor(0.16f, 0.94f, 0.94f);
+    static final Color PURPLE = Color.getHSBColor(0.8f, 1, 0.71f);
+    static final Color RED = Color.getHSBColor(0, 1, 1);
+    static final Color GREEN = Color.getHSBColor(0.34f, 0.88f, 0.88f);
+    static final Color BULE = Color.getHSBColor(0.66f, 1, 1);
+    static final Color ORANGE = Color.getHSBColor(0.09f, 1, 1);
+    static final Color[] COLORS = { BLACK, SKYBLUE, YELLOW, PURPLE, RED, GREEN, BULE, ORANGE };
+}
