5e0af71c70f91b821d9fb52b3880aaa8d8d3ba2d
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-01-14 00:20:19 +0900

    gapの計算タイミングを修正

--- app/src/main/java/jp/ac/uryukyu/ie/e205701/NextMinoPanel.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205701/NextMinoPanel.java
@@ -28,18 +28,17 @@ public class NextMinoPanel extends JPanel {
     public void paint(Graphics g) {
         int loop = 0;
         for (Mino nextMino : nextMinos) {
+            int gap = BLOCK_SIZE / 2;
+            if (nextMino.shape[0].length % 4 == 0) {
+                gap = 0;
+            } else if (nextMino.shape[0].length % 2 == 0) {
+                gap = BLOCK_SIZE;
+            }
             Color blockColor = ColorSet.COLORS[nextMino.COLOR_NUM];
             for (int i = 0; i < nextMino.shape[0].length; i++) {
                 for (int j = 0; j < nextMino.shape[0][0].length; j++) {
                     if (nextMino.shape[0][i][j] == 0)
                         continue;
-
-                    int gap = BLOCK_SIZE / 2;
-                    if (nextMino.shape[0].length % 4 == 0) {
-                        gap = 0;
-                    } else if (nextMino.shape[0].length % 2 == 0) {
-                        gap = BLOCK_SIZE;
-                    }
 
                     g.setColor(blockColor);
                     g.fillRect(j * BLOCK_SIZE + gap, i * BLOCK_SIZE + loop * BLOCK_SIZE * 4, BLOCK_SIZE, BLOCK_SIZE);
